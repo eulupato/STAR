46292229327eee5995e599dbccf13f3bd56d5f1e
eulupato <nlupato09@gmail.com>
2026-08-30 12:52:14 -0300

    v1.9: exportar somente componentes de voz locais

--- voice/__init__.py
+++ voice/__init__.py
@@ -1,1 +1,4 @@
-from .elevenlabs_voice import ElevenLabsVoice
+from .manager import VoiceManager, LocalSpeechToText, ChatterboxTTS
+from .audio_input import AudioRecorder
+
+__all__ = ["VoiceManager", "LocalSpeechToText", "ChatterboxTTS", "AudioRecorder"]
