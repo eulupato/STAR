483ede20b2df2bee532304b253d58b9d5350cf4a
eulupato <nlupato09@gmail.com>
2026-08-31 12:20:15 -0300

    v1.9: declarar preferência de voz rápida local

--- config.py
+++ config.py
@@ -17,7 +17,8 @@ VOICE_CLONE_ENGINE = "Chatterbox"
 VOICE_MODE = "official"
 
 # Motor rápido disponível somente quando escolhido explicitamente.
-VOICE_FAST_ENGINE = "Piper"
+VOICE_FAST_ENGINE = "Windows SAPI / Piper"
+VOICE_FAST_PREFERENCE = "sapi"
 VOICE_FALLBACK_ON_ERROR = False
 
 # O caminho padrão é privado/local. O gerenciador também detecta
