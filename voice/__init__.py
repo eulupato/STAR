86c264b52d12c76cae10f49eda98438764d357bc
eulupato <nlupato09@gmail.com>
2026-09-09 22:40:49 -0300

    refactor(voice): keep Seed-VC import fully lazy

--- voice/__init__.py
+++ voice/__init__.py
@@ -1,11 +1,4 @@
 from .manager import VoiceManager, LocalSpeechToText
 from .audio_input import AudioRecorder
-from .seed_vc import SeedVCBackend, SeedVCError
 
-__all__ = [
-    "VoiceManager",
-    "LocalSpeechToText",
-    "AudioRecorder",
-    "SeedVCBackend",
-    "SeedVCError",
-]
+__all__ = ["VoiceManager", "LocalSpeechToText", "AudioRecorder"]
