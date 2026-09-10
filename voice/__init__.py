a90f7d4bf32cb1150820058828b2d0581e58e3da
eulupato <nlupato09@gmail.com>
2026-09-09 22:37:33 -0300

    feat(voice): expose Seed-VC backend

--- voice/__init__.py
+++ voice/__init__.py
@@ -1,4 +1,11 @@
 from .manager import VoiceManager, LocalSpeechToText
 from .audio_input import AudioRecorder
+from .seed_vc import SeedVCBackend, SeedVCError
 
-__all__ = ["VoiceManager", "LocalSpeechToText", "AudioRecorder"]
+__all__ = [
+    "VoiceManager",
+    "LocalSpeechToText",
+    "AudioRecorder",
+    "SeedVCBackend",
+    "SeedVCError",
+]
